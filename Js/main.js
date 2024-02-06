fac5d1f9902eafd8bbda2526532115e0e98edb01
Carlos A. Aristizabal <71797754+CarlosAndresAristizabal@users.noreply.github.com>
2024-02-06 18:13:38 -0500

    Quitando scrollreveal

--- Js/main.js
+++ Js/main.js
@@ -1,67 +1,30 @@
-const menu = document.querySelector('#navegacion__lista'),
-    activeMenu = document.querySelectorAll('.nav__lista__link'),
-    modal1 = document.querySelector('.abrirModalDiseno'),
-    modal2 = document.querySelector('.abrirModalWeb'),
-    modal3 = document.querySelector('.abrirModal3D'),
-    modalDiseno = document.querySelector('.modalDiseno'),
-    modalWeb = document.querySelector('.modalWeb'),
-    modal3D = document.querySelector('.modal3D'),
-    modal4 = document.querySelector('.abrirModalCrusoUno'),
-    modal5 = document.querySelector('.abrirModalCrusoDos'),
-    modal6 = document.querySelector('.abrirModalCrusoTres'),
-    modal7 = document.querySelector('.abrirModalCrusoCuatro'),
-    modal8 = document.querySelector('.abrirModalCrusoCinco'),
-    modal9 = document.querySelector('.abrirModalCrusoSeis'),
-    modal10 = document.querySelector('.abrirModalCrusoSiete'),
-    modal11 = document.querySelector('.abrirModalCrusoOcho'),
-    modal12 = document.querySelector('.abrirModalCrusoNueve'),
-    modalCursoUno = document.querySelector('.CursoUno'),
-    modalCursoDos = document.querySelector('.CursoDos'),
-    modalCursoTres = document.querySelector('.CursoTres'),
-    modalCursoCuatro = document.querySelector('.CursoCuatro'),
-    modalCursoCinco = document.querySelector('.CursoCinco'),
-    modalCursoSeis = document.querySelector('.CursoSeis'),
-    modalCursoSiete = document.querySelector('.CursoSiete'),
-    modalCursoOcho = document.querySelector('.CursoOcho'),
-    modalCursoNueve = document.querySelector('.CursoNueve'),
-    cerrarModal = document.querySelectorAll('.modal__cerrar'),
-    cerrarModal_curso = document.querySelectorAll('.modal__cerrar_curso');
-
-
-activeMenu.forEach(item => {
-    item.addEventListener('click', e => {
-        activeMenu.forEach(item => {
-            item.classList.remove('active', 'activo');
-            if (screen.width < 600) {
-                item.classList.remove('active');
-            }
-        });
-        item.classList.add('active', 'activo');
-    });
-});
-
-
-ScrollReveal().reveal('.anima_bottom', {
-    duration: 3000,
-    origin: 'bottom',
-    distance: '-100px', reset: true,
-});
-
-ScrollReveal().reveal('.anima_top', {
-    duration: 4000,
-    origin: 'top',
-    distance: '100px', reset: true,
-});
-ScrollReveal().reveal('.anima_left', {
-    duration: 4000,
-    origin: 'left',
-    distance: '-200px', reset: true,
-});
-ScrollReveal().reveal('.anima_rigth', {
-    duration: 4000,
-    origin: 'rigth',
-    distance: '-300px', reset: true,
-});
+const menu = document.querySelector('#navegacion__lista')
+const modal1 = document.querySelector('.abrirModalDiseno')
+const modal2 = document.querySelector('.abrirModalWeb')
+const modal3 = document.querySelector('.abrirModal3D')
+const modalDiseno = document.querySelector('.modalDiseno')
+const modalWeb = document.querySelector('.modalWeb')
+const modal3D = document.querySelector('.modal3D')
+const modal4 = document.querySelector('.abrirModalCrusoUno')
+const modal5 = document.querySelector('.abrirModalCrusoDos')
+const modal6 = document.querySelector('.abrirModalCrusoTres')
+const modal7 = document.querySelector('.abrirModalCrusoCuatro')
+const modal8 = document.querySelector('.abrirModalCrusoCinco')
+const modal9 = document.querySelector('.abrirModalCrusoSeis')
+const modal10 = document.querySelector('.abrirModalCrusoSiete')
+const modal11 = document.querySelector('.abrirModalCrusoOcho')
+const modal12 = document.querySelector('.abrirModalCrusoNueve')
+const modalCursoUno = document.querySelector('.CursoUno')
+const modalCursoDos = document.querySelector('.CursoDos')
+const modalCursoTres = document.querySelector('.CursoTres')
+const modalCursoCuatro = document.querySelector('.CursoCuatro')
+const modalCursoCinco = document.querySelector('.CursoCinco')
+const modalCursoSeis = document.querySelector('.CursoSeis')
+const modalCursoSiete = document.querySelector('.CursoSiete')
+const modalCursoOcho = document.querySelector('.CursoOcho')
+const modalCursoNueve = document.querySelector('.CursoNueve')
+const cerrarModal = document.querySelectorAll('.modal__cerrar')
+const cerrarModal_curso = document.querySelectorAll('.modal__cerrar_curso')
 
 modal1.addEventListener('click', e => {
     e.preventDefault();
