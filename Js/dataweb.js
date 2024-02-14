6540f2fa7d839f5c043788a96b56fb2349595335
Carlos A. Aristizabal <71797754+CarlosAndresAristizabal@users.noreply.github.com>
2024-02-14 16:11:37 -0500

    creando archivo dataweb

---
+++ Js/dataweb.js
@@ -0,0 +1,59 @@
+const dataweb = [
+    {
+        "id": 1,
+        "titulo": "Conoce Como Utilizar CRM para Retener y Fidelizar Clientes.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva1.JPG"
+    },
+    {
+        "id": 2,
+        "titulo": "Cree su Plan de Comunicación Digital.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Cree_su_Plan_de _Comunicacion/Diapositiva1.JPG"
+    },
+    {
+        "id": 3,
+        "titulo": " Maneje Eficientemente sus Inventarios através de la Sistematización.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Maneje_Eficientemente_sus_Invetarios/Diapositiva1.JPG"
+    },
+    {
+        "id": 4,
+        "titulo": "Conozca los Beneficios de Contar con sus Inventarios Automatizados.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Conozca_las_Herramientas_y Los_Beneficios_de_Contar_con_sus_Inventarios_Automatizados/Diapositiva1.JPG"
+    },
+    {
+        "id": 5,
+        "titulo": "Pontencializa el Uso del CRM en tu Empresa.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Potencializa_el_Uso_del_CRM/Diapositiva1.JPG"
+    },
+    {
+        "id": 6,
+        "titulo": "Preste sus Servicios de Forma Automatizada.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Preste_sus_Servicios_de_Forma_Automatizada/Diapositiva1.JPG"
+    },
+    {
+        "id": 7,
+        "titulo": "Como darle Notoriedad a su Marca con el Branded Content.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/storeLine/Mockup_Branded_Content/Diapositiva1.JPG"
+    },
+    {
+        "id": 8,
+        "titulo": "  Como Hacer Contratos con sus Clientes y Proveedores.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/storeLine/Mockup_Como_Hacer_Contratos_con_sus_Clientes _y_Provedores/Diapositiva1.JPG"
+    },
+    {
+        "id": 9,
+        "titulo": "Conoce los Beneficios para Ser una Sociedad BIC.",
+        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "urlImage": "/img/Web/storeLine/Mockup_Conoce_los_Beneficios_para_Ser_una_Sociedad_BIC/Diapositiva1.JPG"
+    },
+
+]
+
+export default dataweb
--- Js/main.js
+++ Js/main.js
@@ -2,155 +2,151 @@ import dataDiseno from './datadiseno.js'
 import data3D from './data3d.js'
 
 const activeMenu = document.querySelectorAll('.nav__lista__link'),
-    modalDiseno = document.querySelector('.modalDiseno'),
-    modal3D = document.querySelector('.modal3D'),
-    modalWeb = document.querySelector(".modalWeb"),
-    modal4 = document.querySelector('.abrirModalCrusoUno'),
-    modal5 = document.querySelector('.abrirModalCrusoDos'),
-    modal6 = document.querySelector('.abrirModalCrusoTres'),
-    modal7 = document.querySelector('.abrirModalCrusoCuatro'),
-    modal8 = document.querySelector('.abrirModalCrusoCinco'),
-    modal9 = document.querySelector('.abrirModalCrusoSeis'),
-    modal10 = document.querySelector('.abrirModalCrusoSiete'),
-    modal11 = document.querySelector('.abrirModalCrusoOcho'),
-    modal12 = document.querySelector('.abrirModalCrusoNueve'),
-    modalCursoUno = document.querySelector('.CursoUno'),
-    modalCursoDos = document.querySelector('.CursoDos'),
-    modalCursoTres = document.querySelector('.CursoTres'),
-    modalCursoCuatro = document.querySelector('.CursoCuatro'),
-    modalCursoCinco = document.querySelector('.CursoCinco'),
-    modalCursoSeis = document.querySelector('.CursoSeis'),
-    modalCursoSiete = document.querySelector('.CursoSiete'),
-    modalCursoOcho = document.querySelector('.CursoOcho'),
-    modalCursoNueve = document.querySelector('.CursoNueve'),
-    cerrarModal_curso = document.querySelectorAll('.modal__cerrar_curso')
+  modalDiseno = document.querySelector('.modalDiseno'),
+  modal3D = document.querySelector('.modal3D'),
+  modalWeb = document.querySelector(".modalWeb"),
+  modal4 = document.querySelector('.abrirModalCrusoUno'),
+  modal5 = document.querySelector('.abrirModalCrusoDos'),
+  modal6 = document.querySelector('.abrirModalCrusoTres'),
+  modal7 = document.querySelector('.abrirModalCrusoCuatro'),
+  modal8 = document.querySelector('.abrirModalCrusoCinco'),
+  modal9 = document.querySelector('.abrirModalCrusoSeis'),
+  modal10 = document.querySelector('.abrirModalCrusoSiete'),
+  modal11 = document.querySelector('.abrirModalCrusoOcho'),
+  modal12 = document.querySelector('.abrirModalCrusoNueve'),
+  modalCursoUno = document.querySelector('.CursoUno'),
+  modalCursoDos = document.querySelector('.CursoDos'),
+  modalCursoTres = document.querySelector('.CursoTres'),
+  modalCursoCuatro = document.querySelector('.CursoCuatro'),
+  modalCursoCinco = document.querySelector('.CursoCinco'),
+  modalCursoSeis = document.querySelector('.CursoSeis'),
+  modalCursoSiete = document.querySelector('.CursoSiete'),
+  modalCursoOcho = document.querySelector('.CursoOcho'),
+  modalCursoNueve = document.querySelector('.CursoNueve')
 
+// ------------- Activating each item in the menu bar----------
 activeMenu.forEach(item => {
-    item.addEventListener('click', () => {
-        activeMenu.forEach(item => {
-            item.classList.remove('active', 'activo');
-            if (screen.width < 600) {
-                item.classList.remove('active');
-            }
-        });
-        item.classList.add('active', 'activo');
+  item.addEventListener('click', () => {
+    activeMenu.forEach(item => {
+      item.classList.remove('activo');
     });
+    item.classList.add('activo');
+  });
 });
-alert('Estamos actualizando')
 
 
 modal4.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoUno.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoUno.classList.add('modal__mostrar');
 });
 modal5.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoDos.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoDos.classList.add('modal__mostrar');
 });
 modal6.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoTres.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoTres.classList.add('modal__mostrar');
 });
 modal7.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoCuatro.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoCuatro.classList.add('modal__mostrar');
 });
 modal8.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoCinco.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoCinco.classList.add('modal__mostrar');
 });
 modal9.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoSeis.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoSeis.classList.add('modal__mostrar');
 });
 modal10.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoSiete.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoSiete.classList.add('modal__mostrar');
 });
 modal11.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoOcho.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoOcho.classList.add('modal__mostrar');
 });
 modal12.addEventListener('click', e => {
-    e.preventDefault();
-    modalCursoNueve.classList.add('modal__mostrar');
+  e.preventDefault();
+  modalCursoNueve.classList.add('modal__mostrar');
 });
 
 
 const itemsDiseno = document.getElementById('cardsDiseno')
 const items3D = document.getElementById('cards3D')
-const cardDiseno = document.getElementById('templateCardsDiseno').content
+const cardDiseno = document.getElementById('templateCardsProyecto').content
 const fragment = document.createDocumentFragment()
 const slider = document.querySelector(".slider__content__img");
 
 document.addEventListener('click', e => {
 
-    // -------------- click open modals of the project--------
-    const modals = e.target.dataset.id
-    const quitarClass = document.querySelector(" .modal__mostrar")
+  // -------------- click open modals of the project--------
+  const modals = e.target.dataset.id
+  const quitarClass = document.querySelector(" .modal__mostrar")
 
-    console.log('aca', e.target)
-    if (modals == 'tresD') {
-        modal3D.classList.add('modal__mostrar');
-        e.stopPropagation()
-        pintarCards3D(data3D)
-    } if (modals == 'web') {
-        e.stopPropagation()
-        modalWeb.classList.add('modal__mostrar');
-    } if (modals == 'diseno') {
-        modalDiseno.classList.add('modal__mostrar');
-        e.stopPropagation()
-        pintarCardsDiseno(dataDiseno)
-    }
-    //--------------------- click close modals of the project-------------------
-    if (e.target.matches('.modal__cerrar')) {
-        quitarClass.classList.remove("modal__mostrar")
-    }
-    if (e.target.matches('.modal__cerrar_curso')) {
-        modalWeb.classList.add('modal__mostrar');
-        quitarClass.classList.remove("modal__mostrar")
-    }
-    // ------------------next and prev of the courses-----------
-    if (e.target.classList.contains('button-prev')) {
-        slider.scrollLeft -= 500
-        // e.target.classList.contains('button-prev')
-        e.stopPropagation()
-    }
-    if (e.target.classList.contains('button-next')) {
-        slider.scrollLeft += 500
-        // e.target.classList.contains('button-next')
-        e.stopPropagation()
-    }
+  if (modals == 'tresD') {
+    modal3D.classList.add('modal__mostrar');
+    pintarCards3D(data3D)
     e.stopPropagation()
+  } if (modals == 'web') {
+    modalWeb.classList.add('modal__mostrar');
+    e.stopPropagation()
+  } if (modals == 'diseno') {
+    modalDiseno.classList.add('modal__mostrar');
+    pintarCardsDiseno(dataDiseno)
+    e.stopPropagation()
+  }
+  //--------------------- click close modals of the project-------------------
+  if (e.target.matches('.modal__cerrar')) {
+    quitarClass.classList.remove("modal__mostrar")
+  }
+  if (e.target.matches('.modal__cerrar_curso')) {
+    modalWeb.classList.add('modal__mostrar');
+    quitarClass.classList.remove("modal__mostrar")
+  }
+  // ------------------next and prev of the courses-----------
+  if (e.target.classList.contains('button-prev')) {
+    slider.scrollLeft -= 500
+    // e.target.classList.contains('button-prev')
+    e.stopPropagation()
+  }
+  if (e.target.classList.contains('button-next')) {
+    slider.scrollLeft += 500
+    // e.target.classList.contains('button-next')
+    e.stopPropagation()
+  }
+  e.stopPropagation()
 
 })
 
 // creation of an arrow function for the section of the cards of the design projects through an object and using a template
 const pintarCardsDiseno = dataDiseno => {
-    const datos = dataDiseno
-    datos.forEach(itemsDiseno => {
-        cardDiseno.querySelector('figcaption').textContent = itemsDiseno.titulo;
-        cardDiseno.querySelector('img').setAttribute('data-src', itemsDiseno.urlImagen)
-        cardDiseno.querySelector('img').setAttribute('alt', itemsDiseno.titulo)
+  const datos = dataDiseno
+  datos.forEach(itemsDiseno => {
+    cardDiseno.querySelector('figcaption').textContent = itemsDiseno.titulo;
+    cardDiseno.querySelector('img').setAttribute('data-src', itemsDiseno.urlImagen)
+    cardDiseno.querySelector('img').setAttribute('alt', itemsDiseno.titulo)
 
-        const clone = cardDiseno.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-    itemsDiseno.appendChild(fragment)
+    const clone = cardDiseno.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  itemsDiseno.appendChild(fragment)
 
 }
 // creation of an arrow function for the section of the cards of the 3D design  projects through an object and using a template
 const pintarCards3D = data3D => {
-    const datos = data3D
-    datos.forEach(items3D => {
-        cardDiseno.querySelector('figcaption').textContent = items3D.titulo;
-        cardDiseno.querySelector('img').setAttribute('data-src', items3D.urlImagen)
-        cardDiseno.querySelector('img').setAttribute('alt', items3D.titulo)
+  const datos = data3D
+  datos.forEach(items3D => {
+    cardDiseno.querySelector('figcaption').textContent = items3D.titulo;
+    cardDiseno.querySelector('img').setAttribute('data-src', items3D.urlImagen)
+    cardDiseno.querySelector('img').setAttribute('alt', items3D.titulo)
 
-        const clone = cardDiseno.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-    items3D.appendChild(fragment)
+    const clone = cardDiseno.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  items3D.appendChild(fragment)
 
 }
+
 
