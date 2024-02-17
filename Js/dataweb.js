af683e2c47b54ff35777965b35aff0b372aca6a7
Carlos A. Aristizabal <71797754+CarlosAndresAristizabal@users.noreply.github.com>
2024-02-17 00:14:30 -0500

    CORRECIONES Y ACTUALIACION DE DATOS

--- Js/dataweb.js
+++ Js/dataweb.js
@@ -2,56 +2,75 @@ const dataweb = [
     {
         "id": 1,
         "titulo": "Conoce Como Utilizar CRM para Retener y Fidelizar Clientes.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva1.JPG"
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
+        "galeria": [
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva1.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva2.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva3.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva4.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva5.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva6.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva7.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva8.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva9.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva10.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva11.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva12.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva13.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva14.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva15.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva16.JPG",
+            "/img/Web/htmlCSsJs/Mockup_Conoce_Como_Utilizar_CRM_Para_Retener__y_Fidelizar_Clientes/Diapositiva17.JPG"
+        ]
     },
     {
         "id": 2,
         "titulo": "Cree su Plan de Comunicación Digital.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/htmlCSsJs/Mockup_Cree_su_Plan_de _Comunicacion/Diapositiva1.JPG"
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Cree_su_Plan_de _Comunicacion/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 3,
         "titulo": " Maneje Eficientemente sus Inventarios através de la Sistematización.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/htmlCSsJs/Mockup_Maneje_Eficientemente_sus_Invetarios/Diapositiva1.JPG"
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Maneje_Eficientemente_sus_Invetarios/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 4,
         "titulo": "Conozca los Beneficios de Contar con sus Inventarios Automatizados.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/htmlCSsJs/Mockup_Conozca_las_Herramientas_y Los_Beneficios_de_Contar_con_sus_Inventarios_Automatizados/Diapositiva1.JPG"
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Conozca_las_Herramientas_y Los_Beneficios_de_Contar_con_sus_Inventarios_Automatizados/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 5,
         "titulo": "Pontencializa el Uso del CRM en tu Empresa.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/htmlCSsJs/Mockup_Potencializa_el_Uso_del_CRM/Diapositiva1.JPG"
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Potencializa_el_Uso_del_CRM/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 6,
         "titulo": "Preste sus Servicios de Forma Automatizada.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/htmlCSsJs/Mockup_Preste_sus_Servicios_de_Forma_Automatizada/Diapositiva1.JPG"
+        "urlImage": "/img/Web/htmlCSsJs/Mockup_Preste_sus_Servicios_de_Forma_Automatizada/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 7,
         "titulo": "Como darle Notoriedad a su Marca con el Branded Content.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/storeLine/Mockup_Branded_Content/Diapositiva1.JPG"
+        "urlImage": "/img/Web/storeLine/Mockup_Branded_Content/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 8,
-        "titulo": "  Como Hacer Contratos con sus Clientes y Proveedores.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/storeLine/Mockup_Como_Hacer_Contratos_con_sus_Clientes _y_Provedores/Diapositiva1.JPG"
+        "titulo": "Como Hacer Contratos con sus Clientes y Proveedores.",
+        "urlImage": "/img/Web/storeLine/Mockup_Como_Hacer_Contratos_con_sus_Clientes _y_Provedores/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
     {
         "id": 9,
         "titulo": "Conoce los Beneficios para Ser una Sociedad BIC.",
-        "descripion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>.",
-        "urlImage": "/img/Web/storeLine/Mockup_Conoce_los_Beneficios_para_Ser_una_Sociedad_BIC/Diapositiva1.JPG"
+        "urlImage": "/img/Web/storeLine/Mockup_Conoce_los_Beneficios_para_Ser_una_Sociedad_BIC/Diapositiva1.JPG",
+        "descripcion": "Este proyecto es un curso virtual que esta realizado para la <strong>Cámara de Comercio de Bogotá</strong> por medio de la empresa <strong>Virtual Technologies S.A.S</strong>."
     },
 
 ]
--- Js/main.js
+++ Js/main.js
@@ -1,29 +1,20 @@
 import dataDiseno from './datadiseno.js'
 import data3D from './data3d.js'
+import dataweb from './dataweb.js'
 
-const activeMenu = document.querySelectorAll('.nav__lista__link'),
-  modalDiseno = document.querySelector('.modalDiseno'),
-  modal3D = document.querySelector('.modal3D'),
-  modalWeb = document.querySelector(".modalWeb"),
-  modal4 = document.querySelector('.abrirModalCrusoUno'),
-  modal5 = document.querySelector('.abrirModalCrusoDos'),
-  modal6 = document.querySelector('.abrirModalCrusoTres'),
-  modal7 = document.querySelector('.abrirModalCrusoCuatro'),
-  modal8 = document.querySelector('.abrirModalCrusoCinco'),
-  modal9 = document.querySelector('.abrirModalCrusoSeis'),
-  modal10 = document.querySelector('.abrirModalCrusoSiete'),
-  modal11 = document.querySelector('.abrirModalCrusoOcho'),
-  modal12 = document.querySelector('.abrirModalCrusoNueve'),
-  modalCursoUno = document.querySelector('.CursoUno'),
-  modalCursoDos = document.querySelector('.CursoDos'),
-  modalCursoTres = document.querySelector('.CursoTres'),
-  modalCursoCuatro = document.querySelector('.CursoCuatro'),
-  modalCursoCinco = document.querySelector('.CursoCinco'),
-  modalCursoSeis = document.querySelector('.CursoSeis'),
-  modalCursoSiete = document.querySelector('.CursoSiete'),
-  modalCursoOcho = document.querySelector('.CursoOcho'),
-  modalCursoNueve = document.querySelector('.CursoNueve')
-
+const activeMenu = document.querySelectorAll('.nav__lista__link')
+const modalDiseno = document.querySelector('.modalDiseno')
+const modal3D = document.querySelector('.modal3D')
+const modalWeb = document.querySelector(".modalWeb")
+const modalCurso = document.querySelector(".modalCurso")
+const itemsDiseno = document.getElementById('cardsDiseno')
+const items3D = document.getElementById('cards3D')
+const itemsWeb = document.getElementById('cardsWeb')
+const cardDiseno = document.getElementById('templateCardsProyecto').content
+const cardWeb = document.getElementById('templateCardsWeb').content
+const fragment = document.createDocumentFragment()
+// const slider = document.querySelector(".slider__content__img");
+let galeria = []
 // ------------- Activating each item in the menu bar----------
 activeMenu.forEach(item => {
   item.addEventListener('click', () => {
@@ -35,55 +26,12 @@ activeMenu.forEach(item => {
 });
 
 
-modal4.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoUno.classList.add('modal__mostrar');
-});
-modal5.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoDos.classList.add('modal__mostrar');
-});
-modal6.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoTres.classList.add('modal__mostrar');
-});
-modal7.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoCuatro.classList.add('modal__mostrar');
-});
-modal8.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoCinco.classList.add('modal__mostrar');
-});
-modal9.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoSeis.classList.add('modal__mostrar');
-});
-modal10.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoSiete.classList.add('modal__mostrar');
-});
-modal11.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoOcho.classList.add('modal__mostrar');
-});
-modal12.addEventListener('click', e => {
-  e.preventDefault();
-  modalCursoNueve.classList.add('modal__mostrar');
-});
-
-
-const itemsDiseno = document.getElementById('cardsDiseno')
-const items3D = document.getElementById('cards3D')
-const cardDiseno = document.getElementById('templateCardsProyecto').content
-const fragment = document.createDocumentFragment()
-const slider = document.querySelector(".slider__content__img");
 
 document.addEventListener('click', e => {
 
   // -------------- click open modals of the project--------
   const modals = e.target.dataset.id
-  const quitarClass = document.querySelector(" .modal__mostrar")
+  const quitarClass = document.querySelector(".modal__mostrar")
 
   if (modals == 'tresD') {
     modal3D.classList.add('modal__mostrar');
@@ -91,11 +39,16 @@ document.addEventListener('click', e => {
     e.stopPropagation()
   } if (modals == 'web') {
     modalWeb.classList.add('modal__mostrar');
+    pintarCardsWeb(dataweb)
     e.stopPropagation()
   } if (modals == 'diseno') {
     modalDiseno.classList.add('modal__mostrar');
     pintarCardsDiseno(dataDiseno)
     e.stopPropagation()
+  }
+  if (e.target.classList.contains('btnImagen')) {
+    modalCurso.classList.add('modal__mostrar')
+    pintarCurso(e.target.parentElement)
   }
   //--------------------- click close modals of the project-------------------
   if (e.target.matches('.modal__cerrar')) {
@@ -105,19 +58,11 @@ document.addEventListener('click', e => {
     modalWeb.classList.add('modal__mostrar');
     quitarClass.classList.remove("modal__mostrar")
   }
-  // ------------------next and prev of the courses-----------
-  if (e.target.classList.contains('button-prev')) {
-    slider.scrollLeft -= 500
-    // e.target.classList.contains('button-prev')
-    e.stopPropagation()
-  }
-  if (e.target.classList.contains('button-next')) {
-    slider.scrollLeft += 500
-    // e.target.classList.contains('button-next')
-    e.stopPropagation()
-  }
-  e.stopPropagation()
 
+  // --------------capture the course by means of the id --------------
+
+
+  e.stopPropagation()
 })
 
 // creation of an arrow function for the section of the cards of the design projects through an object and using a template
@@ -125,9 +70,8 @@ const pintarCardsDiseno = dataDiseno => {
   const datos = dataDiseno
   datos.forEach(itemsDiseno => {
     cardDiseno.querySelector('figcaption').textContent = itemsDiseno.titulo;
-    cardDiseno.querySelector('img').setAttribute('data-src', itemsDiseno.urlImagen)
+    cardDiseno.querySelector('img').setAttribute('src', itemsDiseno.urlImagen)
     cardDiseno.querySelector('img').setAttribute('alt', itemsDiseno.titulo)
-
     const clone = cardDiseno.cloneNode(true)
     fragment.appendChild(clone)
   })
@@ -139,9 +83,8 @@ const pintarCards3D = data3D => {
   const datos = data3D
   datos.forEach(items3D => {
     cardDiseno.querySelector('figcaption').textContent = items3D.titulo;
-    cardDiseno.querySelector('img').setAttribute('data-src', items3D.urlImagen)
+    cardDiseno.querySelector('img').setAttribute('src', items3D.urlImagen)
     cardDiseno.querySelector('img').setAttribute('alt', items3D.titulo)
-
     const clone = cardDiseno.cloneNode(true)
     fragment.appendChild(clone)
   })
@@ -149,4 +92,54 @@ const pintarCards3D = data3D => {
 
 }
 
+// Creation of an arrow function for section of the cards of the web design  projects through an object and using a template
+const pintarCardsWeb = dataweb => {
 
+  // console.log(dataweb)
+  const datosweb = dataweb
+  datosweb.forEach(itemsWeb => {
+    cardWeb.querySelector('h2').textContent = itemsWeb.titulo;
+    cardWeb.querySelector('.parrafo').innerHTML = itemsWeb.descripcion;
+    cardWeb.querySelector('img').setAttribute('src', itemsWeb.urlImage)
+    cardWeb.querySelector('img').setAttribute('alt', itemsWeb.titulo)
+    cardWeb.querySelector('.btnImagen').dataset.id = itemsWeb.id
+    // cardWeb.querySelector('.modal__titulo').textContent = 'Web'
+    galeria = itemsWeb.galeria
+    const clone = cardWeb.cloneNode(true)
+    fragment.appendChild(clone)
+
+  })
+
+  itemsWeb.appendChild(fragment)
+
+}
+
+
+const pintarCurso = item => {
+  // console.log(item)
+
+  const cursoGaleria = {
+    titulo: item.querySelector('h2').textContent,
+    id: item.querySelector('.btnImagen').dataset.id,
+  }
+  let element = cursoGaleria.id
+
+}
+
+
+
+
+
+
+//   // ------------------next and prev of the courses-----------
+//   if (e.target.classList.contains('button-prev')) {
+//     slider.scrollLeft -= 500
+//     e.target.classList.contains('button-prev')
+//     e.stopPropagation()
+//   }
+//   if (e.target.classList.contains('button-next')) {
+//     slider.scrollLeft += 500
+//     e.target.classList.contains('button-next')
+//     e.stopPropagation()
+//   }
+//   e.stopPropagation()
